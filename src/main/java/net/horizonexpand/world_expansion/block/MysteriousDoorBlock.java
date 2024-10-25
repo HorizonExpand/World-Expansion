6a47774b0785aceb2390890a9e4884702cca52a4
Hrumer <sema.yakushkov@gmail.com>
2024-10-25 16:51:07 +0300

    Fixed bugs and other changes
    ~The "Mysterious Door" is now opening
    - Added translation of the JEI category for a "Multi-Crafting Table"
    - Fixed the mod's tags directory
    ~The tag "world_expansion:shotgun_bullet" was removed because it was unnecessary
    ~The tag "forge:mud_ruin_based_to" has been replaced with "world_expansion:mud_ruins_based_to"
    ~The "Mud Ruin0" structure is no longer generated on the Beach biome
    - Dump files have been deleted
    ~The description of the mod has been changed

--- src/main/java/net/horizonexpand/world_expansion/block/MysteriousDoorBlock.java
+++ src/main/java/net/horizonexpand/world_expansion/block/MysteriousDoorBlock.java
@@ -1,7 +1,6 @@
 
 package net.horizonexpand.world_expansion.block;
 
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
@@ -9,15 +8,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.DoorBlock;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
-
-import net.horizonexpand.world_expansion.procedures.Mysterious_Door_ActionProcedure;
 
 public class MysteriousDoorBlock extends DoorBlock {
 	public MysteriousDoorBlock() {
@@ -32,19 +25,5 @@ public class MysteriousDoorBlock extends DoorBlock {
 	@Override
 	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
 		return 20;
-	}
-
-	@Override
-	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
-		super.use(blockstate, world, pos, entity, hand, hit);
-		int x = pos.getX();
-		int y = pos.getY();
-		int z = pos.getZ();
-		double hitX = hit.getLocation().x;
-		double hitY = hit.getLocation().y;
-		double hitZ = hit.getLocation().z;
-		Direction direction = hit.getDirection();
-		Mysterious_Door_ActionProcedure.execute(entity);
-		return InteractionResult.SUCCESS;
 	}
 }
