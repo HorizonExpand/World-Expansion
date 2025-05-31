cffea7e7f8f8a7b78d90c793e873cc473d6d49f0
Hrumer <sema.yakushkov@gmail.com>
2025-06-01 00:02:33 +0300

    Little Grass
    - Now "Little Grass" bonemeable (grow to "Short Grass")
    - Now "Little Grass" growing when "Bone Meal" used

--- src/main/java/net/horizonexpand/world_expansion/block/LittleGrassBlock.java
+++ src/main/java/net/horizonexpand/world_expansion/block/LittleGrassBlock.java
@@ -14,16 +14,22 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.GrassColor;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.util.RandomSource;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.client.renderer.BiomeColors;
 
+import net.horizonexpand.world_expansion.procedures.LittleGrassBoneMealUseProcedure;
 import net.horizonexpand.world_expansion.init.WorldExpansionModBlocks;
 
-public class LittleGrassBlock extends FlowerBlock {
+public class LittleGrassBlock extends FlowerBlock implements BonemealableBlock {
 	public LittleGrassBlock() {
 		super(MobEffects.DAMAGE_RESISTANCE, 50, BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).sound(SoundType.GRASS).instabreak().noCollission().replaceable().offsetType(BlockBehaviour.OffsetType.XZ).pushReaction(PushReaction.DESTROY));
 	}
@@ -42,6 +48,21 @@ public class LittleGrassBlock extends FlowerBlock {
 	@Override
 	public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
 		return 60;
+	}
+
+	@Override
+	public boolean isValidBonemealTarget(LevelReader worldIn, BlockPos pos, BlockState blockstate) {
+		return true;
+	}
+
+	@Override
+	public boolean isBonemealSuccess(Level world, RandomSource random, BlockPos pos, BlockState blockstate) {
+		return true;
+	}
+
+	@Override
+	public void performBonemeal(ServerLevel world, RandomSource random, BlockPos pos, BlockState blockstate) {
+		LittleGrassBoneMealUseProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
 	}
 
 	@OnlyIn(Dist.CLIENT)
